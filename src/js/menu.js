db3f29588ba6cc224a9d316cc09e7ab732a74638
TeaRediT <vlad.bobro.02@gmail.com>
2025-09-03 00:05:14 +0300

    update menu js

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,10 +3,17 @@
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
     modal: document.querySelector('[data-menu]'),
+    menuLinks: document.querySelectorAll('[data-menu] a[href^="#"]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      toggleModal();
+    });
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
